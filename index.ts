721bd60875ff41c4cfef240e6eb047e40c985899
ANZIE05 <161513339+ANZIE05@users.noreply.github.com>
2024-04-19 01:34:42 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+let user_response = await inquirer.prompt({
+     type: "input",
+     name: "words",
+     message: "Enter your sentence please",
+})
+
+let word_count = user_response.words.trim().split(" ").length
+
+console.log(`Your sentence has ${word_count} words.`);
